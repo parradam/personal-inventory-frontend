29680c8e2d05ad679788995ee00ec7bacf158435
Adam Parr <adamparr.uk@gmail.com>
2024-09-29 13:14:13 +0100

    Ensure register, login pages redirect to ItemList when logged in

--- frontend/src/routes/login.lazy.tsx
+++ frontend/src/routes/login.lazy.tsx
@@ -1,6 +1,11 @@
+import NotAuthWrapperRedirect from '@/auth/components/NotAuthWrapperRedirect';
 import LoginPage from '@/auth/pages/LoginPage';
 import { createLazyFileRoute } from '@tanstack/react-router';
 
 export const Route = createLazyFileRoute('/login')({
-  component: () => <LoginPage />,
+  component: () => (
+    <NotAuthWrapperRedirect to="/items">
+      <LoginPage />,
+    </NotAuthWrapperRedirect>
+  ),
 });
